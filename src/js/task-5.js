7fed712eb22dc4c2593136940717d1edd2974308
Valeri <mylenovo21072017@gmail.com>
2024-07-29 19:38:21 +0300

    all tasks

--- src/js/task-5.js
+++ src/js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  document.querySelector('.color').textContent = randomColor;
+}
+
+document.querySelector('.change-color').addEventListener('click', changeColor);
