fab5ec298c57732661cb58863f994f168299a060
Valeri <mylenovo21072017@gmail.com>
2024-07-29 20:29:33 +0300

    заново все розархывувала

--- src/js/task-6.js
+++ src/js/task-6.js
@@ -3,38 +3,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-function createBoxes(amount) {
-  const boxesContainer = document.getElementById('boxes');
-  const boxSizeStep = 10;
-  const initialSize = 30;
-  boxesContainer.innerHTML = '';
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    const size = initialSize + i * boxSizeStep;
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.border = '1px solid black';
-    boxesContainer.appendChild(box);
-  }
-}
-
-function destroyBoxes() {
-  const boxesContainer = document.getElementById('boxes');
-  boxesContainer.innerHTML = '';
-}
-
-document.querySelector('[data-create]').addEventListener('click', () => {
-  const input = document.querySelector('#controls input');
-  const amount = parseInt(input.value, 10);
-
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-  }
-
-  input.value = '';
-});
-
-document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
